3c159c044280f586e3dcecae036ec0841eab5c8c
Andrew Sutiono <sutionoandrew@gmail.com>
2026-02-12 11:48:28 +0700

    Finalization and filtering feature

--- Acceloka.Api/Features/Tickets/GetAvailableTickets/GetAvailableTicketsHandler.cs
+++ Acceloka.Api/Features/Tickets/GetAvailableTickets/GetAvailableTicketsHandler.cs
@@ -13,35 +13,73 @@ namespace Acceloka.Api.Features.Tickets.GetAvailableTickets
         {
             this._dbContext = dbContext;
         }
-        public  async Task<List<GetAvailableTicketsResponse>> Handle(GetAvailableTicketsQuery request, CancellationToken cancellationToken)
+        public async Task<List<GetAvailableTicketsResponse>> Handle(GetAvailableTicketsQuery request, CancellationToken cancellationToken)
         {
             //checkig on pages
             var skip = (request.page - 1) * request.pageSize;
 
-            var tickets = await _dbContext.Tickets
-            .Include(t => t.Category)
-            .Include(t => t.BookedTicketDetails)
-            .Where(t =>
-                t.Quota >
-                (t.BookedTicketDetails.Sum(d => (int?)d.Quantity) ?? 0)
-            )
-            .OrderBy(t => t.NamaTiket)
-            .Skip(skip)
-            .Take(request.pageSize)
-            .Select(t => new GetAvailableTicketsResponse
-            {
-                EventDate = t.EventDate.ToString("dd/MM/yyyy HH:mm:ss"),
-                TicketCode = t.KodeTiket,
-                TicketName = t.NamaTiket,
-                Price = t.Harga,
-                Category = t.Category.Name,
-                Quota =
-                    t.Quota -
-                    (t.BookedTicketDetails.Sum(d => (int?)d.Quantity) ?? 0)
-            })
-            .ToListAsync(cancellationToken);
+            var query = _dbContext.Tickets
+         .Include(t => t.Category)
+         .Include(t => t.BookedTicketDetails)
+         .AsQueryable();
 
-            return tickets;
+            query.Where(t => t.Quota > (t.BookedTicketDetails.Sum(d => (int?)d.Quantity) ?? 0));
+
+            //filtering
+            query = HandleFilterRequest(request, query);
+            //sorting
+            query = HandleSortRequest(request, query);
+
+            return await query
+                .Skip(skip)
+                .Take(request.pageSize)
+                .Select(t => new GetAvailableTicketsResponse
+                {
+                    EventDate = t.EventDate.ToString("dd/MM/yyyy HH:mm:ss"),
+                    TicketCode = t.KodeTiket,
+                    TicketName = t.NamaTiket,
+                    Price = t.Harga,
+                    Category = t.Category.Name,
+                    Quota = t.Quota - (t.BookedTicketDetails.Sum(d => (int?)d.Quantity) ?? 0)
+                })
+                .ToListAsync(cancellationToken);
+        }
+
+        private static IQueryable<Domain.Entities.Ticket> HandleSortRequest(GetAvailableTicketsQuery request, IQueryable<Domain.Entities.Ticket> query)
+        {
+            var orderBy = string.IsNullOrWhiteSpace(request.OrderBy) ? "TicketCode" : request.OrderBy;
+            var orderState = string.IsNullOrWhiteSpace(request.OrderState) ? "asc" : request.OrderState.ToLower();
+
+            query = orderBy switch
+            {
+                "TicketName" => orderState == "desc" ? query.OrderByDescending(t => t.NamaTiket) : query.OrderBy(t => t.NamaTiket),
+                "Category" => orderState == "desc" ? query.OrderByDescending(t => t.Category.Name) : query.OrderBy(t => t.Category.Name),
+                "Price" => orderState == "desc" ? query.OrderByDescending(t => t.Harga) : query.OrderBy(t => t.Harga),
+                _ => orderState == "desc" ? query.OrderByDescending(t => t.KodeTiket) : query.OrderBy(t => t.KodeTiket)
+            };
+            return query;
+        }
+
+        private static IQueryable<Domain.Entities.Ticket> HandleFilterRequest(GetAvailableTicketsQuery request, IQueryable<Domain.Entities.Ticket> query)
+        {
+            if (!string.IsNullOrWhiteSpace(request.CategoryName))
+                query = query.Where(t => t.Category.Name.Contains(request.CategoryName));
+
+            if (!string.IsNullOrWhiteSpace(request.TicketCode))
+                query = query.Where(t => t.KodeTiket.Contains(request.TicketCode));
+
+            if (!string.IsNullOrWhiteSpace(request.TicketName))
+                query = query.Where(t => t.NamaTiket.Contains(request.TicketName));
+
+            if (request.MaxPrice.HasValue)
+                query = query.Where(t => t.Harga <= request.MaxPrice.Value);
+
+            if (request.StartEventDate.HasValue)
+                query = query.Where(t => t.EventDate >= request.StartEventDate.Value);
+
+            if (request.EndEventDate.HasValue)
+                query = query.Where(t => t.EventDate <= request.EndEventDate.Value);
+            return query;
         }
     }
 }
--- Acceloka.Api/Features/Tickets/GetAvailableTickets/Requests/GetAvailableTicketsQuery.cs
+++ Acceloka.Api/Features/Tickets/GetAvailableTickets/Requests/GetAvailableTicketsQuery.cs
@@ -7,5 +7,17 @@ namespace Acceloka.Api.Features.Tickets.GetAvailableTickets.Requests
     {
         public int page { get; set; } = 1;
         public int pageSize { get; set; } = 10;
+
+        //sorting
+        public string? OrderBy { get; set; }
+        public string? OrderState { get; set; }
+
+        //searching
+        public string? CategoryName { get; set; }
+        public string? TicketCode { get; set; }
+        public string? TicketName { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public DateTime? StartEventDate { get; set; }
+        public DateTime? EndEventDate { get; set; }
     }
 }
